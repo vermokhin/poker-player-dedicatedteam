a23222b998fed8f9c20404ad1f0c98a88ab416e8
Alexey Kuznetsov <a.kuznetsov@simbirsoft.com>
2016-04-24 17:29:20 +0400

    improve selector

--- src/BetSelector.cs
+++ src/BetSelector.cs
@@ -28,8 +28,15 @@ namespace Nancy.Simple
 				case 0:
 				case 1:
 				case 2:
-					return GetCallBet(_player.stack, _state.current_buy_in, _player.bet);
-				case 3:
+			    {
+			        var distinctRanks = cards.Select(c => c.rank).Distinct();
+			        if (distinctRanks.Count() > 4)
+			        {
+			            return 0;
+			        }
+			        return GetCallBet(_state.current_buy_in, _player.bet);
+			    }
+			    case 3:
 					return SelectBetFor3Cards(cards);
 				case 4:
 					return SelectBetFor4Cards(cards);
@@ -76,13 +83,9 @@ namespace Nancy.Simple
 			return SelectBetFor4Cards(cards);
 		}
 
-        int GetCallBet(int stack, int currentBet, int ourBet)
+        int GetCallBet(int currentBet, int ourBet)
         {
             var needToAdd = currentBet > ourBet ? currentBet - ourBet : 0;
-            if (stack - needToAdd < 300)
-            {
-                return 0;
-            }
             return needToAdd;
         }
     }
