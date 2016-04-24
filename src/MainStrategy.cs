f52f257687fffad32a60eaa05ff8105a57c02037
Vitaliy Ermokhin <v.ermohin@simbirsoft.com>
2016-04-24 10:34:56 +0000

    update strategy

--- src/MainStrategy.cs
+++ src/MainStrategy.cs
@@ -13,24 +13,41 @@ namespace Nancy.Simple
 		public int CalculateBet(GameState state)
 		{
 			var player = state.players.FirstOrDefault(p => p.hole_cards.Length > 0);
-			var cards = player.hole_cards;
+			var cards = state.community_cards.Concat(player.hole_cards);
+
+			var doubleBet = state.current_buy_in * 2;
+			var minRaiseBet = state.current_buy_in + state.minimum_raise;
 			if (cards != null)
 			{
 				//get cards combination
 				var hand = cards.Select(c => c.rank).ToString().ToLower();
-				if (hand.First().Equals(hand.Last()))
+				if(cards.Count() < 3)
 				{
-					var result = 0;
-					if(int.TryParse(hand.First().ToString(), out result))
+					if(state.current_buy_in > player.bet)
 					{
-						if (result < minCardRank)
-							return 0;
+						return state.current_buy_in - player.bet;
 					}
-					var bet = state.current_buy_in * 2;
-					return player.stack > bet ? bet : player.stack;
+					return 0;
 				}
+
+				//no pairs in cards
+				if((cards.Select(c=>c.rank).Distinct().Count() == cards.Count()) ||
+					(cards.Select(c=>c.suit).Distinct().Count() == cards.Count()))
+				{
+					return doubleBet > player.stack ? player.stack : doubleBet;
+				}
+				else
+				{
+					return minRaiseBet > player.stack ? player.stack : minRaiseBet;
+				}
+
+				if(cards.Count() == 5)
+				{
+					//use rank api
+				}
+				
 			}
-			return 0;
+			return state.minimum_raise;
 		}
 
 		private enum HandsType
