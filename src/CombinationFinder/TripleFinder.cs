88d9fb38f42409088cd5337d28421dec304bdbbd
Alexey Kuznetsov <a.kuznetsov@simbirsoft.com>
2016-04-24 17:08:59 +0400

    change triple finder

--- src/CombinationFinder/TripleFinder.cs
+++ src/CombinationFinder/TripleFinder.cs
@@ -5,12 +5,12 @@ namespace Nancy.Simple.CombinationFinder
 {
 	class TripleFinder
 	{
-	    int GetTriple(IEnumerable<int> ranks)
+	    double GetTriple(IEnumerable<int> ranks)
 	    {
 	        var triple = ranks.GroupBy(r => r).FirstOrDefault(g => g.Count() == 3);
 	        if (triple != null)
 	        {
-	            return triple.First();
+	            return 0.1 * triple.First();
 	        }
 	        return 0;
 	    }
