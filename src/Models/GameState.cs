6986204ac27072471f08365a84674915d4b2f3e4
Vitaliy Ermokhin <v.ermohin@simbirsoft.com>
2016-04-24 07:01:16 +0000

    Add models

---
+++ src/Models/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple.Models
+{
+	class GameState
+	{
+		public Player[] players { get; set; }
+		public string tournament_id { get; set; }
+		public string game_id { get; set; }
+		public int round { get; set; }
+		public int bet_index { get; set; }
+		public int small_blind { get; set; }
+		public int orbits { get; set; }
+		public int dealer { get; set; }
+		public object[] community_cards { get; set; }
+		public int current_buy_in { get; set; }
+		public int pot { get; set; }
+	}
+}
---
+++ src/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple.Models
+{
+	public class Player
+	{
+		public string name { get; set; }
+		public int stack{get; set; }
+		public string status { get; set; }
+		public int bet { get; set; }
+		public object [] hole_cards { get; set; }
+		public string version { get; set; }
+		public int id { get; set; }
+
+	}
+}
